1c6963d84c467576ecbb2d05f0ddd67f74638bd7
thenameispavan <78902050+thenameispavan@users.noreply.github.com>
2021-02-20 16:12:27 +0530

    Add files via upload

---
+++ lab4/Inheritance/src/com/cg/eis/pl/EmpMain.java
@@ -0,0 +1,9 @@
+package com.cg.eis.pl;
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ lab4/Inheritance/src/com/cg/eis/service/Service.java
@@ -0,0 +1,10 @@
+package com.cg.eis.service;
+
+public class Service implements EmployeeService{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
